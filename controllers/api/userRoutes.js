928a3bb16919ba5da0872ccf4c5dcabc9ef7eab5
Supriya Sharma <sharmasup565@gmail.com>
2022-06-01 10:55:17 -0700

    implement logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/signup', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
 
@@ -22,16 +22,12 @@ router.post('/login', async (req, res) => {
             where: { username: req.body.username }
         });
 
-        console.log(userData);
-
         if (!userData) {
             res.status(400).json({ message: 'Incorrect username or password' });
             return;
         }
 
         const validPassword = await userData.checkPassword(req.body.password);
-
-        console.log(validPassword);
 
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect username or password' });
@@ -46,6 +42,16 @@ router.post('/login', async (req, res) => {
         });
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
     }
 });
 
--- public/js/login.js
+++ public/js/login.js
@@ -11,8 +11,6 @@ const loginFormHandler = async (event) => {
             headers: { 'Content-Type': 'application/json' }
         });
 
-        console.log(response);
-
         if (response.ok) {
             document.location.replace('/dashboard');
         } else {
